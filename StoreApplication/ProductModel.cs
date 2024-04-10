191788acfa9b6261c930203fc90131f5e62f4ddf
kreativniynick <kreativniynick@gmail.com>
2024-04-10 16:59:16 +0600

    Implement basic store page

---
+++ StoreApplication/ProductModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+
+namespace StoreApplication
+{
+    public class ProductModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private decimal _price;
+        private bool _isInCart;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        public bool IsInCart
+        {
+            get { return _isInCart; }
+            set
+            {
+                _isInCart = value;
+                OnPropertyChanged(nameof(IsInCart));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ StoreApplication/StoreViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace StoreApplication
+{
+    public class StoreViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ProductModel> Products { get; set; }
+        public ICommand AddToCartCommand { get; set; }
+
+        public StoreViewModel()
+        {
+            Products = new ObservableCollection<ProductModel>()
+            {
+                new() {Name = "Product 1", Price = 10},
+                new() {Name = "Product 2", Price = 15},
+                new() {Name = "Product 3", Price = 15},
+                new() {Name = "Product 4", Price = 15},
+                new() {Name = "Product 5", Price = 15},
+                new() {Name = "Product 6", Price = 15},
+            };
+
+            AddToCartCommand = new RelayCommand(AddToCart);
+        }
+
+        private void AddToCart(object obj)
+        {
+            if (obj is ProductModel product)
+                product.IsInCart = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
