31e8771c6c3a3c36d7f7a3f1b1d75b2810d07bee
kreativniynick <kreativniynick@gmail.com>
2024-04-10 13:25:50 +0600

    Add custom control NavigationButton

---
+++ StoreApplication/NavigationButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace StoreApplication
+{
+    public class NavigationButton : ListBoxItem
+    {
+        static NavigationButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavigationButton), new FrameworkPropertyMetadata(typeof(NavigationButton)));
+        }
+
+        public string NavigationPath
+        {
+            get { return (string)GetValue(NavigationPathProperty); }
+            set { SetValue(NavigationPathProperty, value); }
+        }
+
+        public static readonly DependencyProperty NavigationPathProperty =
+            DependencyProperty.Register("NavigationPath", typeof(string), typeof(NavigationButton), new PropertyMetadata(null));
+
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(NavigationButton), new PropertyMetadata(null));
+    }
+}
